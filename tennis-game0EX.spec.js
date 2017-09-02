f880bf50f4aab7ea7dd4bd9822912bba88f6bc4a
nattapong <58160388@go.buu.ac.th>
2017-09-02 16:21:06 +0700

    tennis-game0EX.spec.js

--- tennis-game0EX.spec.js
+++ tennis-game0EX.spec.js
@@ -1,17 +1,22 @@
 function TennisGame() {
     const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
-    this.playerAScore = 0
+    this.playerAScore
+    this.playerBScore
 
     this.reset = () => {
         this.playerAScore = 0
+        this.playerBScore = 0
     }
 
     this.echo = () => {
-       return `${scoreString[this.playerAScore]} - Love`
+       return `${scoreString[this.playerAScore]} - ${scoreString[this.playerBScore]}`
     }
 
     this.playerAGteScore = () => {
         this.playerAScore++
+    }
+    this.playerBGteScore = () => {
+        this.playerBScore++
     }
 }
 
@@ -38,4 +43,31 @@ test('Echo "Fifteen - Love" when playerA get first score', () => {
 
     // Assert
     expect(result).toBe('Fifteen - Love')
+})
+test('Echo "Love - Fifteen" when playerA get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerBGteScore()
+
+    // Act
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Love - Fifteen')
+})
+
+test('Echo "Thirty - Fourty" when playerA get Two score and playerB get T score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerAScore = 2
+    app.playerBScore = 3
+
+
+    // Act
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Thirty - Forthy')
 })
